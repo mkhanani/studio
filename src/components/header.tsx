79c48b4f316fb71d464f6c62a5e4b3ce0e7866fd
Mustafa Khanani <m.khanani69@gmail.com>
2025-07-24 15:46:42 +0000

    Can you make sure that we don't have bugs anymore? I physically replaced

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,9 +4,9 @@ import { SidebarTrigger } from "@/components/ui/sidebar"
 export function Header() {
   return (
     <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background/80 px-4 backdrop-blur-sm sm:px-6">
-       <SidebarTrigger className="flex md:hidden"/>
+       <SidebarTrigger className="flex"/>
        <div className="flex-1">
-         <h1 className="font-headline text-lg font-semibold">GridAI</h1>
+         {/* This empty div is used to push the UserNav to the right */}
        </div>
       <div className="flex items-center gap-4">
         <UserNav />
